0c9466469e0dd84d87446e08f264f8381600c183
Edward Tate <edward.tate@erlang-solutions.com>
2013-09-11 13:32:57 +0100

    Removed all warnings, clarified output.

--- src/tcache.erl
+++ src/tcache.erl
@@ -6,8 +6,9 @@
 -export([find/5, add/6, collect/0]).
 
 %% gen_server API exports
--export([start_link/1, init/1, terminate/2]).
+-export([start_link/1]).
 -export([handle_call/3, handle_cast/2, handle_info/2]).
+-export([code_change/3, init/1, terminate/2]).
 
 %%------------------------------------------------------------------------------
 %% State
@@ -64,19 +65,21 @@ handle_call(collect, _From, S0) ->
 %%------------------------------------------------------------------------------
 %% internal
 %%------------------------------------------------------------------------------
-find_update(X, K, D, W0, T, S0) ->
+find_update(X, K, D, W0, _T, S0) ->
   Ko = lists:keysort(1, K),
   Do = lists:keysort(1, D),
   case ets:lookup(S0#state.data, {X,Ko,Do}) of
     [] ->
-%%      io:format("Adding ~p ~p ~p = ~p~n", [X,Ko,Do,{calc,W0}]),      
+      Ks = [{Key,Ord} || {{_,Key},Ord} <- Ko],
+      Ds = [Key || {_,Key} <- Do],
+      io:format("Adding ~p ~p ~p = ~p~n", [X,Ks,Ds,{calc,W0}]),
       true = ets:insert_new(S0#state.data, {{X,Ko,Do}, {calc, W0}}),
       S2 = S0#state{ck = S0#state.ck + 1},
       {reply, {{calc, W0}, S2#state.ck},  S2};
     [{_, {calc, W1} = V}] ->
-      case W1 < W0 of
+      case W1 =< W0 of
 	true ->
-	  io:format("Thread ~p is greater than or equal to ~p~n", [W1, W0]),
+	  io:format("Thread ~p is less than or equal to ~p~n", [W1, W0]),
 	  {reply, hang, S0};
 	false ->
 	  {reply, {V, S0#state.ck}, S0}
@@ -88,24 +91,26 @@ find_update(X, K, D, W0, T, S0) ->
       {reply, hang, S0}
   end.
 
-add_update(X, K, D, W, T, V1, S0) ->
+add_update(X, K, D, W, _T, V1, S0) ->
   Ko = lists:keysort(1, K),
   Do = lists:keysort(1, D),
   case ets:lookup(S0#state.data, {X,Ko,Do}) of
     [] ->
       {reply, hang, S0};
-    [{_, {calc, W} = V0}] ->
-      io:format("Adding ~p ~p ~p = ~p~n", [X,Ko,Do,V1]),
+    [{_, {calc, W}}] ->
+      Ks = [{Key,Ord} || {{_,Key},Ord} <- Ko],
+      Ds = [Key || {_,Key} <- Do],
+      io:format("Adding ~p ~p ~p = ~p~n", [X,Ks,Ds,V1]),
       ets:insert(S0#state.data, {{X,Ko,Do}, V1}),
       S1 = S0#state{ck = S0#state.ck + 1},
       {reply, {V1, S1#state.ck}, S1};
-    [{_, {calc, W1} = V0}] ->
+    [{_, {calc, _W1}}] ->
       {reply, hang, S0};
-    [O1,O2|Os] ->
+    [O1,_O2|_Os] ->
       io:format("Multiple objects with the same key ~p ~p~n", [W, O1]),
       {reply, hang, S0};
     Other ->
-      [{{X1,K1,D1},V1}] = Other,
+      [{{_X1,_K1,_D1},V1}] = Other,
       io:format("Other = ~p, W = ~p~n", [Other, W]),
       {reply, hang, S0}
   end.
@@ -113,6 +118,9 @@ add_update(X, K, D, W, T, V1, S0) ->
 %%------------------------------------------------------------------------------
 %% Not implemented
 %%------------------------------------------------------------------------------
+code_change(_, _, _) ->
+  not_implemented.
+
 handle_cast(_, _) ->
   not_implemented.
 
