401d7dc5dd58880929efce5568519736033c21ac
Pierre Fenoll <pierrefenoll@gmail.com>
2013-09-16 00:54:53 +0100

    Splitted tests to use parser and interface, also in a more rebar way.

--- test/tea_tests.erl
+++ test/tea_tests.erl
@@ -6,42 +6,95 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+
 %% API tests.
 
-%% Parallel, one-dimensional (tournament) wheredim clause (introducing parallelism)
+const_test() ->
+    %% replace with qc generators
+    MaxN = 100000000000000000000,
+    tcache:start_link(100),
+    [begin
+        N = random:uniform(MaxN),
+        {ok, Tree} = tea:string(integer_to_list(N)),
+        ?assertMatch({N, _},
+            tcore:eval(Tree, fun(X)->X end,[],[], [], self(), 0))
+    end || _ <- lists:seq(1,10)].
 
-t1_test () ->
-    {ok, E6} = tea:string(
-        "// Tournament in 1 dimension
-        A
-        where
-            dim t <- 2
-            dim s <- 0
+string_test () ->
+    {ok, Tree} = tea:string("`Test`"),
+    S = {string,"Test"},
+    tcache:start_link(100),
+    ?assertMatch({S, _},
+        tcore:eval(Tree, fun(X)->X end,[],[], [], self(), 0)).
 
-            // Compute A across space
-            var A =
-                if #.t <= 0 then
-                    B
-                else
-                    (A @ [s <- #.s * 2] + A @ [s <- #.s * 2 + 1]) @ [t <- #.t - 1]
-                fi
+constant_dim_test () ->
+    {ok, Tree} = tea:string("#.t"),
+    K = [{{[0],"t"}, 100}],
+    D = [{[0],"t"}],
+    tcache:start_link(100),
+    ?assertMatch({100, _},
+        tcore:eval(Tree, [],[],K, D, [0], 0)).
 
-            // Ensure spatial values are between 1 and 1024
-            var B =
-               if #.s >= 1 and #.s <= 1024 then
-                    #.s
-                else
-                    1
-                fi
-        end"),
-    I = [],
-    E = [],
-    K = [],
-    TimeD = {[0],time},
-    SpaceD = {[0],space},
+tuple1_test () ->
+    {ok, Tree} = tea:string(" [t <- 1, s <- 2] "),
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    K = [{TimeD,100}, {SpaceD,100}],
     D = [TimeD,SpaceD],
     tcache:start_link(100),
-    ?assertMatch({7,_}, tcore:eval(E6, I, E, K, D, [0], 0)).
-    % {badmatch,_} = (catch tcore:eval(bla, I, E, K, D, [0], 0)). % kills the cache.
+    ?assertMatch({{te,[{TimeD,1},{SpaceD,2}]}, _}, % Uh? ‘te’? Is just ‘t’ the same?
+        tcore:eval(Tree, [],[],K, D, [0], 0)).
+
+tuple2_test () ->
+    {ok, Tree} = tea:string(" [#.t <- 0, #.s <- 1] "),
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    tcache:start_link(100),
+    ?assertMatch({[TimeD,SpaceD], _},
+        tcore:eval(Tree, fun(X)->X end,[],[], [], self(), 0)).
+
+primop1_test () ->
+    {ok, Tree} = tea:string(" 10 + 20 "),
+    tcache:start_link(100),
+    ?assertMatch({30, _},
+        tcore:eval(Tree, fun(X)->X end,[],[], [], self(), 0)).
+
+primop2_test () ->
+    {ok, Tree} = tea:string(" #.t + #.s "),
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    K = [{TimeD,100}, {SpaceD,100}],
+    tcache:start_link(100),
+    ?assertMatch({[TimeD,SpaceD], _},
+        tcore:eval(Tree, [],[],K, [], [0], 0)).
+
+primop3_test () ->
+    {ok, Tree} = tea:string(" #.t + #.s "),
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    K = [{TimeD,100}, {SpaceD,100}],
+    D = [SpaceD, TimeD],
+    tcache:start_link(100),
+    ?assertMatch({200, _},
+        tcore:eval(Tree, [],[],K, D, [0], 0)).
+
+perturb_test () ->
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    K = [{TimeD,100}, {SpaceD,100}],
+    {ok, E1} = tea:string(" #.s @ [s <- 0] "),
+    ?assertMatch({0, _},
+        tcore:eval(E1, [], [], K, [SpaceD], [0], 0)),
+    {ok, E2} = tea:string(" #.t @ [s <- 0] "),
+    ?assertMatch({[TimeD], _},
+        tcore:eval(E2, [], [], [], [], [0], 0)),
+    {ok, E3} = tea:string(" #.t @ [s <- 0, t <- 1] "),
+    ?assertMatch({1, _},
+        tcore:eval(E3, [], [], K, [TimeD], [0], 0)),
+    {ok, E4} = tea:string(" #.t @ [t <- 1, s <- 0] "),
+    ?assertMatch({1, _},
+        tcore:eval(E4, [], [], K, [TimeD], [0], 0)).
+
+%% Internals
 
 %% End of Module.
---
+++ test/wheredim_string_tests.erl
@@ -0,0 +1,234 @@
+%% See LICENSE for licensing information.
+%% -*- coding: utf-8 -*-
+-module(wheredim_string_tests).
+
+%% tea_string_tests: tests for function tea:string/1.
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+%% API tests.
+
+e1_test () ->
+    TimeD = {[0],"t"},
+    {ok, Tree} = tea:string(
+        "X
+        where
+            dim t <- 0
+            var X = #.t
+        end"),
+    ?assertEqual(
+        {wheredim,
+            {wherevar, "X",
+                [{"X", {'#', TimeD}}]},
+            [{TimeD, 0}]},
+        Tree).
+
+e2_test () ->
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    {ok, Tree} = tea:string(
+        "#.t + #.s
+        where
+            dim t <- 1
+        end"),
+    ?assertEqual(
+        {wheredim,
+            plus({'#',TimeD}, {'#',SpaceD}),
+            [{TimeD, 1}]},
+        Tree).
+
+e3_test () ->
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    {ok, Tree} = tea:string(
+        "#.t + #.s
+        where
+            dim t <- 2
+            dim s <- 3
+        end"),
+    ?assertEqual(
+        {wheredim,
+            plus({'#',TimeD}, {'#',SpaceD}),
+            [{TimeD,2}, {SpaceD,3}]},
+        Tree).
+
+e4_test () ->
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    {ok, Tree} = tea:string(
+        "X
+        where
+            var X = #.t + #.s
+            dim t <- 2
+            dim s <- 3
+        end"),
+    ?assertEqual(
+        {wheredim, 
+            {wherevar, "X",
+                [{"X",
+                    plus({'#',TimeD}, {'#',SpaceD})}]},
+            [{TimeD,2}, {SpaceD, 3}]},
+        Tree).
+
+e5_test () ->
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    {ok, Tree} = tea:string(
+        "N0
+        where
+            var N0 = if #.t == 0
+                     then N1
+                     else N0 @ [t <- #.t - 1]
+                     fi
+            var N1 = if #.s == 0
+                     then 1
+                     else N1 @ [s <- #.s - 1] * 2
+                     fi
+            dim t <- 10
+            dim s <- 10
+        end"),
+    ?assertEqual(
+        {wheredim, 
+            {wherevar, "N0",
+                [
+                    {"N0",
+                        {'if',
+                            eq({'#', TimeD}, 0),
+                            "N1",
+                            {'@', "N0",
+                                {t, [{TimeD, minus({'#', TimeD}, 1)}]}}}},
+                    {"N1",
+                        {'if',
+                            eq({'#', SpaceD}, 0),
+                            1,
+                            times({'@', "N1", {t, [{SpaceD, minus({'#', SpaceD}, 1)}]}}
+                                , 2)
+                        }}
+                ]},
+            [{TimeD, 10}, {SpaceD, 10}]},
+        Tree).
+
+e6_test () ->
+    TimeD = {[0],"t"},
+    SpaceD = {[0],"s"},
+    {ok, Tree} = tea:string(
+        "// Tournament in 1 dimension
+        A
+        where
+            dim t <- 2
+            dim s <- 0
+
+            // Compute A across space
+            var A =
+                if #.t <= 0 then
+                    B
+                else
+                    (A @ [s <- #.s * 2] + A @ [s <- #.s * 2 + 1]) @ [t <- #.t - 1]
+                fi
+
+            // Ensure spatial values are between 1 and 1024
+            var B =
+               if #.s >= 1 and #.s <= 1024 then
+                    #.s
+                else
+                    1
+                fi
+        end"),
+    ?assertEqual(
+        {wheredim,
+            {wherevar, "A", [
+            {"A", {'if',
+                    lte({'#', TimeD}, 0),
+                    "B",
+                    {'@',
+                        plus({'@', "A", {t, [{SpaceD,      times({'#', SpaceD}, 2    )}]}},
+                             {'@', "A", {t, [{SpaceD, plus(times({'#', SpaceD}, 2), 1)}]}}),
+                        {t, [{TimeD, minus({'#', TimeD}, 1)}]}
+                    }
+                }
+            },
+            {"B", {'if',
+                    tand(gte({'#', SpaceD}, 1),
+                         lte({'#', SpaceD}, 1024)),
+                    {'#', SpaceD},
+                    1
+                }
+            }
+            ]},
+            [{TimeD, 2}, {SpaceD, 0}]},
+        Tree).
+
+e7_test () ->
+    TimeD = {[0],"t"},
+    XD = {[0], "x"},
+    YD = {[0], "y"},
+    {ok, Tree} = tea:string(
+        "Y1 @ [x <- 0]
+        where
+            var Y1 = if #.t <= 0 then X1
+                     else ((Y1 @ [x <- #.x * 2,     y <- #.y * 2]
+                         +  Y1 @ [x <- #.x * 2 + 1, y <- #.y * 2])
+                         + (Y1 @ [x <- #.x * 2,     y <- #.y * 2 + 1]
+                         +  Y1 @ [x <- #.x * 2 + 1, y <- #.y * 2 + 1])
+                          ) @ [t <- #.t - 1]
+                     fi
+            var X1 = if (#.x >= 1 and #.y <= 1024) and   ///Mind the .y
+                        (#.y >= 1 and #.y <= 1024)
+                     then #.x
+                     else 1
+                     fi
+            dim t <- 2
+            dim x <- 0
+            dim y <- 0
+        end"),
+    ?assertEqual(
+        {wheredim,
+            {wherevar,
+                {'@', "Y1", {t, [{XD,0}]}},
+                [
+                    {"Y1",
+                        {'if',
+                            lte({'#', TimeD}, 0),
+                            "X1",
+                            {'@',
+                                plus(
+                                    plus({'@', "Y1", {t, [{XD, times({'#', XD}, 2)},
+                                                          {YD, times({'#', YD}, 2)}]}},
+                                         {'@', "Y1", {t, [{XD, plus(times({'#', XD}, 2), 1)},
+                                                          {YD, times({'#', YD}, 2)}]}}),
+                                    plus({'@', "Y1", {t, [{XD, times({'#', XD}, 2)}, 
+                                                          {YD, plus(times({'#', YD}, 2), 1)}]}},
+                                         {'@', "Y1", {t, [{XD, plus(times({'#', XD}, 2), 1)}, 
+                                                          {YD, plus(times({'#', YD}, 2), 1)}]}})
+                                ),
+                                {t, [{TimeD, minus({'#', TimeD}, 1)}]}}}},
+                    {"X1",
+                        {'if',
+                            tand(
+                                tand(gte({'#', XD}, 1), lte({'#', YD}, 1024)),
+                                tand(gte({'#', YD}, 1), lte({'#', YD}, 1024))),
+                            {'#', XD},
+                            1}}
+                ]},
+            [{TimeD,2}, {XD,0}, {YD,0}]},
+        Tree).
+
+%% Internals
+
+eq (A, B) ->
+  {primop, fun erlang:'=='/2, [A, B]}.
+tand (A, B) ->
+  {primop, fun erlang:'and'/2, [A, B]}.
+lte (A, B) ->
+  {primop, fun erlang:'=<'/2, [A, B]}.
+gte (A, B) ->
+  {primop, fun erlang:'>='/2, [A, B]}.
+plus (A, B) ->
+  {primop, fun erlang:'+'/2, [A,B]}.
+times (A, B) ->
+  {primop, fun erlang:'*'/2, [A,B]}.
+minus (A, B) ->
+  {primop, fun erlang:'-'/2, [A, B]}.
+
+%% End of Module.
