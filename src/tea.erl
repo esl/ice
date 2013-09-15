4f52519057aa4c12ffb10bf0aa105141572f4f72
Pierre Fenoll <pierrefenoll@gmail.com>
2013-09-15 17:15:43 +0100

    Removed the bit of redundancy the interface had inherited.

--- src/tea.erl
+++ src/tea.erl
@@ -29,29 +29,25 @@ file (Filename) ->
 rework_tree (Tree) ->
     V = fun
         ({where, _, Exp, DimDecls, VarDecls}) ->
-            TopExpr = id(Exp),
+            TopExpr = Exp,
             Vars = case VarDecls of
                 [] ->
                     TopExpr;
                 VarDecls ->
                     {wherevar, TopExpr,
-                        [{id(Var), E} || {var_decl,_,Var,E} <- VarDecls]}
+                        [{Var, E} || {var_decl,_,Var,E} <- VarDecls]}
             end,
             {wheredim, Vars,
-                [{{[0],id(Dim)},int(N)} || {dim_decl,_,Dim,N} <- DimDecls]};
+                [{{[0],Dim},N} || {dim_decl,_,Dim,N} <- DimDecls]};
 
         ({'if', _, [If|_], Else}) ->
+            % What about elsifs* ?
             {if_expr,_,Cond,Then} = If,
             {'if', Cond, Then, Else};
 
         ({'#.', _, Val}) ->
-            Dim = case Val of
-                "s" ->        {[0],"s"};
-                "t" ->        {[0],"t"};
-                {id,_,"s"} -> {[0],"s"};
-                {id,_,"t"} -> {[0],"t"}
-            end,
-            {'#', Dim};
+            % Assumption: Val is always a dimention (Grammar allows expr).
+            {'#', {[0],Val}};
 
         ({tuple, _, Assocs}) -> {t, Assocs};
         ({tuple_element, _, Lhs, Rhs}) -> {{[0],Lhs}, Rhs};
@@ -67,27 +63,21 @@ rework_tree (Tree) ->
         ({'+'=Op, _, A, B})   -> {primop, fun erlang:Op/2, [A,B]};
         ({'-'=Op, _, A, B})   -> {primop, fun erlang:Op/2, [A,B]};
         ({'*'=Op, _, A, B})   -> {primop, fun erlang:Op/2, [A,B]};
-        ({'%', _, A, B})      -> {primop, fun mod/2, [A,B]};
+        % No '/' yet because floats.
+            %% http://stackoverflow.com/a/858649/1418165
+        ({'%', _, A, B})      -> {primop, fun (X,Y) -> (X rem Y + Y) rem Y end, [A,B]};
 
         ({raw_string, _, S})    -> {string, S};
         ({cooked_string, _, S}) -> {string, S};
 
         ({'@', _, A, B}) -> {'@', A, B};
 
-        ({int,_,_}=Int) -> int(Int);
-        ({id,_,_}=Id) -> id(Id)
+        ({int,_,N}) -> N;
+        ({id,_,Name}) -> Name
     end,
     case tvisitor:visit(V, Tree, bottom_up) of
         [{expr,_,TheWhereDim}] -> {ok, TheWhereDim}
         ; Else -> {ok, Else}
     end.
-
-id ({id, _, Name}) -> Name;
-id (Name) -> Name.
-
-int ({int, _, N}) -> N;
-int (N) -> N.
-
-mod (X, Y) -> (X rem Y + Y) rem Y. %% http://stackoverflow.com/a/858649/1418165
 
 %% End of Module.
