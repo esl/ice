353e0c8471f7a2267c5ddf17af3e6c12834b8d8e
Edward Tate <edward.tate@erlang-solutions.com>
2013-09-19 13:26:52 +0100

    Added where to wheredim / wherevar transform.

--- src/ttransform0.erl
+++ src/ttransform0.erl
@@ -7,14 +7,19 @@
 -export([test/0]).
 
 %%------------------------------------------------------------------------------
-%% Transform0 is responsible for transforming functions only. We define the
-%% transformation pass over the entire parse tree 'just in case', but 
-%% realistically defining functions in certain places should not be legal.
+%% This transform0 module is responsible for transforming functions into the
+%% appropriate abstractions (base / value / name) and in transforming where
+%% clauses into wheredim / wherevar clauses.
+%% We define the transformation pass over the entire parse tree for the sake of
+%% completeness, but clearly some transformations can result in illegal code.
 %% These cases could be specialized in the future if necessary to give us better
 %% error reporting.
 %%
 %% Function AST should look like this:
 %% {fn, X, [{b_param, BArg} | {v_param, VArg} | {n_param, NArg}], E}
+%%
+%% Where AST should look like this:
+%% {where, E0, [{dim, DName, DVal}, {var, VName, VVal}]}
 %%------------------------------------------------------------------------------
 
 %%------------------------------------------------------------------------------
@@ -54,11 +59,15 @@ transform0({'if', E0, E1, E2}) ->
   {'if', transform0(E0), transform0(E1), transform0(E2)};
 
 %%------------------------------------------------------------------------------
-%% Wherevar
+%% Where
 %%------------------------------------------------------------------------------
-transform0({wherevar, E0, XiEis}) ->
-  {wherevar, transform0(E0), lists:map(fun ({Xi,Ei}) -> {Xi, transform0(Ei)} end,
-					    XiEis)};
+transform0({where, E0, VDisEis}) ->
+  %% We should probably signal an error when the body contains other elements..
+  Vars = [{Xi,transform0(Ei)} || {var,Xi,Ei} <- VDisEis],
+  Dims = [{Xi,transform0(Ei)} || {dim,Xi,Ei} <- VDisEis],
+  {wheredim, 
+   {wherevar, transform0(E0), Vars},
+   Dims};
 
 %%------------------------------------------------------------------------------
 %% Dimensional Query
@@ -80,13 +89,6 @@ transform0({b_abs, Is, Params, E}) ->
 
 transform0({b_apply, E0, E1}) ->
   {b_apply, transform0(E0), transform0(E1)};
-
-%%------------------------------------------------------------------------------
-%% Wheredim
-%%------------------------------------------------------------------------------
-transform0({wheredim, E0, XiEis}) ->
-  {wheredim, transform0(E0), lists:map(fun ({Xi,Ei}) -> {Xi, transform0(Ei)} end,
-				      XiEis)};
 
 %%------------------------------------------------------------------------------
 %% Identifiers
@@ -111,15 +113,30 @@ transform0_prime([{n_param, Param}|Ps], E) ->
 %% Instant Tests - Please improve these
 %%------------------------------------------------------------------------------
 d1_tournament() ->
+  %%------------------------------------------------------------------------------
+  %% The following function should be equivalent to the following:
+  %%
+  %% fun tournament.d.lim X = Y
+  %% where
+  %%   var Y = 
+  %%     if #.t <= 0 then 
+  %%       X 
+  %%     else
+  %%       (Y @ [d <- #.d * 2 + 1] + Y @ [d <- #.d * 2]) @ [t <- #.t - 1]
+  %%     fi
+  %% end
+  %%------------------------------------------------------------------------------
+
   {fn, "tournament", [{b_param, d}, {b_param, lim}, {n_param, "X"}],
-   {wherevar, "Y",
-    [{"Y", 
+   {where, "Y",
+    [{var, "Y",
       {'if', tprimop:lte({'#', time}, 0),
        "X",
        {'@',
 	tprimop:plus({'@', "Y", {t, [{d, tprimop:plus(tprimop:times({'#', d}, 2), 1)}]}},
 		     {'@', "Y", {t, [{d, tprimop:times({'#', d}, 2)}]}}),
 	{t, [{time, tprimop:minus({'#', time}, 1)}]}}}}]}}.
+
 
 test() ->
   transform0(d1_tournament()).
