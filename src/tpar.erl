652f4a39516f5a98f1fab6254390f382e699f80e
Luca Favatella <luca.favatella@erlang-solutions.com>
2013-09-20 15:30:06 +0100

    Refactor eval_seq in tpar

--- src/tpar.erl
+++ src/tpar.erl
@@ -31,12 +31,8 @@ eval_seq([], _I, _E, _K, _D, _W, T, Acc) ->
   {lists:reverse(Acc), T};
 eval_seq([X|Xs], I, E, K, D, W, T, Acc) ->
   {D0, T1} = tcore:eval(X, I, E, K, D, W, T),
-  case T1 > T of
-    true ->
-      eval_seq(Xs, I, E, K, D, W, T1, [D0|Acc]);
-    false ->
-      eval_seq(Xs, I, E, K, D, W, T, [D0|Acc])
-  end.
+  NewT = case T1 > T of true -> T1; false -> T end,
+  eval_seq(Xs, I, E, K, D, W, NewT, [D0|Acc]).
 
 
 
