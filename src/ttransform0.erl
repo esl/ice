0500014fb30ccd9ec6f4d39941c5be56e59a62e5
Luca Favatella <luca.favatella@erlang-solutions.com>
2013-10-10 23:25:09 +0100

    Consider functions as variable declarations in where
    Generate function declarations nodes only inside where clauses, as
    already done for (the other) variable declarations.
    
    * Change integration with parser recognizing function declarations
      only in where clauses;
    * Modify transformation 0 accordingly;
    * Transformation 1 does not need changes, as in wherevar the
      expressions assigned to variables are already transformed by
      transformation 1;
    * Update tests.

--- src/ttransform0.erl
+++ src/ttransform0.erl
@@ -65,13 +65,6 @@ transform0({'#', E0}) ->
   {'#', transform0(E0)};
 
 %%------------------------------------------------------------------------------
-%% Function Transformation
-%%------------------------------------------------------------------------------
-transform0({fn, X, Params, E}) ->
-  {wherevar, X,
-   [{X, transform0_prime(Params, transform0(E))}]};
-
-%%------------------------------------------------------------------------------
 %% Base Abstraction
 %%------------------------------------------------------------------------------
 transform0({b_abs, Is, Params, E}) ->
@@ -103,7 +96,10 @@ transform0({i_apply, E}) ->
 %%------------------------------------------------------------------------------
 transform0({where, E0, VDisEis}) ->
   %% We should probably signal an error when the body contains other elements..
-  Vars = [{Xi,transform0(Ei)} || {var,Xi,Ei} <- VDisEis],
+  Vars =
+    [{Xi,transform0(Ei)} || {var,Xi,Ei} <- VDisEis] ++
+    [{Fi,transform0_prime(Params,transform0(E))} %% Function transformation
+     || {fn,Fi,Params,E} <- VDisEis],
   Dims = [{Xi,transform0(Ei)} || {dim,Xi,Ei} <- VDisEis],
   transform0_where(Vars, Dims, E0);
 
--- test/fn_string_tests.erl
+++ test/fn_string_tests.erl
@@ -8,20 +8,24 @@
 %% API tests.
 
 b_abs_test() ->
-  {ok, T} = tea:string("fun base.b = 46"),
-  ExpectedFn = {fn, "base", [{b_param,"b"}], 46},
-  ?assertEqual([ExpectedFn], T).
+  ?assertMatch({where, _,
+                [{fn, "base", [{b_param,"b"}], 46}]},
+               s("58 where fun base.b = 46 end")).
 
 n_abs_test() ->
-  {ok, T} = tea:string("fun named N = 46"),
-  ExpectedFn = {fn, "named", [{n_param,"N"}], 46},
-  ?assertEqual([ExpectedFn], T).
+  ?assertMatch({where, _,
+                [{fn, "named", [{n_param,"N"}], 46}]},
+               s("58 where fun named N = 46 end")).
 
 v_abs_test() ->
-  {ok, T} = tea:string("fun value!v = 46"),
-  ExpectedFn = {fn, "value", [{v_param,"v"}], 46},
-  ?assertEqual([ExpectedFn], T).
+  ?assertMatch({where, _,
+                [{fn, "value", [{v_param,"v"}], 46}]},
+               s("58 where fun value!v = 46 end")).
 
 %% Internals
+
+s(S) ->
+  {ok, T} = tea:string(S),
+  T.
 
 %% End of Module.
