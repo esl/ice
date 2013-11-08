c498712a7d207543b667e56334559e133981f8f9
Luca Favatella <luca.favatella@erlang-solutions.com>
2013-11-08 00:26:37 +0000

    Reorder tea module

--- src/tea.erl
+++ src/tea.erl
@@ -40,20 +40,21 @@ i(String) ->
 
 rework_tree (Tree) ->
   V = fun
-        ({expr, _, E}) ->
-          E;
-        ({call, _, FunExpr, Params}) ->
-          {fn_call, FunExpr, lists:map(fun rework_fun_param/1, Params)};
+        ({expr, _, E}) -> E;
+
+        ({call, _, FunExpr, Params}) -> {fn_call, FunExpr, Params};
 
         ({where, _, Exp, DimDecls, VarDecls}) ->
           TopExpr = Exp,
-          Vars = [{var,Var,E} || {var_decl,_,Var,E} <- VarDecls],
           Dims = [{dim,Dim,N} || {dim_decl,_,Dim,N} <- DimDecls],
-          Funs = [{fn, Name,
-                   lists:map(fun rework_fun_param/1, Params),
-                   Body}
-                  || {fun_decl,_,Name,Params,Body} <- VarDecls],
+          Vars = [{var,Var,E} || {var_decl,_,Var,E} <- VarDecls],
+          Funs = [{fn,Fn,Params,Body}
+                  || {fun_decl,_,Fn,Params,Body} <- VarDecls],
           {where, TopExpr, Vars ++ Dims ++ Funs};
+
+        ({base_param,  _, P}) -> {b_param, P};
+        ({named_param, _, P}) -> {n_param, P};
+        ({value_param, _, P}) -> {v_param, P};
 
         ({'if', _, Ifs, Else}) -> unwrap_elsifs(Ifs, Else);
 
@@ -67,6 +68,8 @@ rework_tree (Tree) ->
           %% On Section 6.4.5 “Tuples” of the TL-doc-0.3.0, tuples are
           %%   defined as a ‘set of (dimension, value) pairs’.
           {{dim,Lhs}, Rhs};
+
+        ({'@', _, A, B}) -> {'@', A, B};
 
         ({'or',  _, A, B}) -> tprimop:tor(A, B);
         ({'and', _, A, B}) -> tprimop:tand(A, B);
@@ -82,15 +85,13 @@ rework_tree (Tree) ->
         ({'/',   _, A, B}) -> tprimop:divide(A, B);
         ({'%',   _, A, B}) -> tprimop:mod(A, B);
 
-        ({bool, _, Boolean}) -> Boolean;
-        ({raw_string, _, S})    -> {string, S};
-        ({cooked_string, _, S}) -> {string, S};
-        ({char, _, Char}) -> {char, Char};
-
-        ({'@', _, A, B}) -> {'@', A, B};
-
         ({int,_,N}) -> N;
         ({float,_,N}) -> N;
+        ({bool, _, Boolean}) -> Boolean;
+        ({char, _, Char}) -> {char, Char};
+        ({raw_string, _, S})    -> {string, S};
+        ({cooked_string, _, S}) -> {string, S};
+
         ({id,_,Name}) -> Name
       end,
   case tvisitor:visit(V, Tree, bottom_up) of
@@ -100,10 +101,6 @@ rework_tree (Tree) ->
       {ok, Y}
   end.
 
-
-rework_fun_param({base_param,  _, P}) -> {b_param, P};
-rework_fun_param({named_param, _, P}) -> {n_param, P};
-rework_fun_param({value_param, _, P}) -> {v_param, P}.
 
 unwrap_elsifs ([{if_expr,_,Cond,Then}|Rest], Else) ->
   {'if', Cond, Then, unwrap_elsifs(Rest,Else)};
