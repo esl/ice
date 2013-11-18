edff72405c1a9d7159ef4410d63765b4e3d0308b
Pierre Fenoll <pierrefenoll@gmail.com>
2013-11-18 11:21:57 +0000

    Fixed uniqueness of string representation of dim id.

--- src/textensional.erl
+++ src/textensional.erl
@@ -109,11 +109,11 @@ eval_ext_cl(DimTypesIn, TypeOut, E0, Ks) ->
 uniq({phi, Id}) ->
   "phi" ++ Id;
 uniq({dim, {Pos,Ix}, Id}) ->
-  Hidden = lists:flatmap(fun integer_to_list/1, [Ix|Pos]),
-  %% TODO Separator must be put between each integer of Pos and Ix, in
-  %% order to distinguish e.g. {dim,{[1,1],11},"t"} from
-  %% {dim,{[1,11],1},"t"} and {dim,{[11,1],1},"t"}
-  "dim" ++ Hidden ++ Id.
+  Hidden = lists:flatmap(
+    fun (X) ->
+      "_" ++ integer_to_list(X)
+    end, [Ix|Pos]),
+  "dim" ++ Hidden ++"_"++ Id.
 
 sanitize(Const) when is_number(Const); is_boolean(Const) ->
   Const;
