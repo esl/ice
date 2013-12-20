b12f0b789da10025a985d7458be0bb5a514275a4
Edward Tate <edward.tate@erlang-solutions.com>
2013-12-20 13:52:06 +0000

    Added ice_cache concurrent interface.

---
+++ src/ice_cache.erl
@@ -0,0 +1,64 @@
+-module(ice_cache).
+
+%% ice_cache interface exports
+-export([create/0, delete/0]).
+-export([find/5, add/6]).
+
+%%------------------------------------------------------------------------------
+%% @doc Create a named ets table which represents the cache
+%%------------------------------------------------------------------------------
+create() ->
+  ice_dtree:new().
+
+%%------------------------------------------------------------------------------
+%% @doc 
+%%------------------------------------------------------------------------------
+delete() ->
+  ice_dtree:delete().
+
+%%------------------------------------------------------------------------------
+%% @doc Find an identifier with a specific context K restricted by the domain D
+%%------------------------------------------------------------------------------
+find(X, K, D, {Id0, _} = W0, _T) ->
+  KD = lists:keysort(1, tset:restrict_domain(K, D)),
+  case ice_dtree:lookup({X,KD}) of
+    [] ->
+%%      io:format(user, "Inserting X = ~p, KD = ~p, {calc, ~p}~n", [X, KD, W0]),
+      true = ice_dtree:insert({X,KD,{calc,W0}}),
+      {{calc,W0}, 0};
+    {calc, {Id1,_} = W1} = V ->
+      case lists:prefix(Id1, Id0) of
+	true ->
+	  hang;
+	false ->
+	  {V, 0}
+      end;
+    V ->
+      {V, 0}
+  end.
+
+%%------------------------------------------------------------------------------
+%% @doc Add an {identifier, context, value} to the cache
+%%------------------------------------------------------------------------------
+add(X, K, D, W, T, V1) ->
+  KD = lists:keysort(1, tset:restrict_domain(K, D)),
+  case ice_dtree:lookup({X,KD}) of
+    [] ->
+      %% 
+      find(X, K, D, W, T),
+      add(X, K, D, W, T, V1);
+    {calc, W} ->
+      case V1 of
+	V1 when is_list(V1) ->
+	  true = ice_dtree:insert({X,KD,{i,V1,[]}});
+	V1 ->
+	  true = ice_dtree:insert({X,KD,V1})
+      end,
+      {V1, 0};
+    {calc, _} = Thr ->
+%%      io:format(user, "X = ~p, KD = ~p is a different thread...~n", [X, KD]),
+      {V1, 0};
+    ExistingV ->
+%%      io:format(user, "Received other = ~p~n", [Other]),
+      {ExistingV, 0}
+  end.
