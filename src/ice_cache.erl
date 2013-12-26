a621162af28c05158443af73ff4d304bda252f41
Luca Favatella <luca.favatella@erlang-solutions.com>
2013-12-26 13:54:59 +0000

    Solve concurrency issue in cache and simplify its semantics
    The cache beta.add() operation was complicated in
    b12f0b789da10025a985d7458be0bb5a514275a4 in order to mitigate a
    concurrency issue.  Solve the root concurrency issue in beta.find()
    and rewrite beta.add() in order to be simpler and more compliant with
    literature.
    
    * Implement atomic find() in the cache using mnesia rather than
      ets. Function ets:insert_new is not enough as, in case the key is
      already present, the value is not returned; a new ets BIF would be
      needed;
    * Fix handling of missing dimensions in ice_cache, clarifying the
      relationship with ice_dtree;
    * Improve (disabled) logging in ice_cache;
    * Update test function in ice_dtree module converting it to eunit
      test.

--- src/ice_cache.erl
+++ src/ice_cache.erl
@@ -21,48 +21,42 @@ delete() ->
 %%------------------------------------------------------------------------------
 find(X, K, D, {Id0, _} = W0, _T) ->
   KD = lists:keysort(1, ice_sets:restrict_domain(K, D)),
-  case ice_dtree:lookup({X,KD}) of
-    [] ->
-%%      io:format(user, "Inserting X = ~p, KD = ~p, {calc, ~p}~n", [X, KD, W0]),
-      true = ice_dtree:insert({X,KD,{calc,W0}}),
-      %% FIXME Lookup must atomically write calc if needed, otherwise
-      %% concurrent find()s for the same {X,KD} would independently
-      %% start computing the same value and write (different) calc,
-      %% violating the assumption of add().
+  case ice_dtree:insert_new({X,KD}, {calc,W0}) of
+    {true, {calc,W0}} ->
+      %% io:format("Inserted X = ~p, KD = ~p, {calc, ~p}~n", [X, KD, W0]),
       {{calc,W0}, 0};
-    {calc, {Id1,_} = W1} = V ->
+    {false, {calc, {Id1,_} = W1} = V} ->
       case lists:prefix(Id1, Id0) of
-	true ->
-	  hang;
-	false ->
-	  {V, 0}
+        true ->
+          hang;
+        false ->
+          %% io:format("Found X = ~p, KD = ~p, {calc, ~p}~n", [X, KD, W1]),
+          {V, 0}
       end;
-    V ->
+    {false, {i,Dims,[]}} ->
+      %% io:format("Found X = ~p, KD = ~p, {i, ~p}~n", [X, KD, Dims]),
+      {Dims, 0};
+    {false, V} ->
+      %% io:format("Found X = ~p, KD = ~p, ~p~n", [X, KD, V]),
       {V, 0}
   end.
 
 %%------------------------------------------------------------------------------
 %% @doc Add an {identifier, context, value} to the cache
 %%------------------------------------------------------------------------------
-add(X, K, D, W, T, V1) ->
+add(X, K, D, W, _T, V1) ->
   KD = lists:keysort(1, ice_sets:restrict_domain(K, D)),
   case ice_dtree:lookup({X,KD}) of
-    [] ->
-      %% 
-      find(X, K, D, W, T),
-      add(X, K, D, W, T, V1);
     {calc, W} ->
       case V1 of
-	V1 when is_list(V1) ->
-	  true = ice_dtree:insert({X,KD,{i,V1,[]}});
-	V1 ->
-	  true = ice_dtree:insert({X,KD,V1})
+        V1 when is_list(V1) ->
+          %% io:format("Inserting X = ~p, KD = ~p, {i, ~p}~n", [X, KD, V1]),
+          true = ice_dtree:insert({X,KD}, {i,V1,[]});
+        V1 ->
+          %% io:format("Inserting X = ~p, KD = ~p, ~p~n", [X, KD, V1]),
+          true = ice_dtree:insert({X,KD}, V1)
       end,
       {V1, 0};
-    {calc, _} = Thr ->
-%%      io:format(user, "X = ~p, KD = ~p is a different thread...~n", [X, KD]),
-      {V1, 0};
-    ExistingV ->
-%%      io:format(user, "Received other = ~p~n", [Other]),
-      {ExistingV, 0}
+    _ ->
+      hang
   end.
--- src/ice_dtree.erl
+++ src/ice_dtree.erl
@@ -1,87 +1,131 @@
 -module(ice_dtree).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -export([new/0, delete/0]).
--export([lookup/1, insert/1]).
--export([test/0]).
+-export([lookup/1,
+         insert/2, insert_new/2]).
 
 -define(TABLE_NAME, ice_cache).
+
+-type id() :: nonempty_string().
+-type dim() :: term().
+-type ground_value() :: term().
+-type context() :: [{dim(), ground_value()}].
+
+-record(?TABLE_NAME,
+        {k :: {id(), context()},
+         v :: ground_value() |
+              {calc, Pid :: term()} |
+              {i, MissingDims :: [dim()], []}}).
 
 %%------------------------------------------------------------------------------
 %% @doc Create a new ets table for the cache
 %%------------------------------------------------------------------------------
 new() ->
-  Tab = ets:new(?TABLE_NAME, [named_table, public, set, 
-			      {read_concurrency, true},
-			      {write_concurrency, true},
-			      {keypos, 1}]),
-  ets:delete_all_objects(Tab),
-  Tab.
+  ok = application:start(mnesia),
+  {atomic, ok} =
+    mnesia:create_table(
+      ?TABLE_NAME,
+      [{attributes, record_info(fields, ?TABLE_NAME)},
+       {disc_copies, []},
+       {disc_only_copies, []},
+       {ram_copies, [node()]},
+       {storage_properties, [{ets, [{read_concurrency, true},
+                                    {write_concurrency, true}]}]},
+       {type, set}]),
+  ok.
 
 %%------------------------------------------------------------------------------
 %% @doc Delete the ets table
 %%------------------------------------------------------------------------------
 delete() ->
-  ets:delete(?TABLE_NAME).
+  {atomic, ok} = mnesia:delete_table(?TABLE_NAME),
+  ok = application:stop(mnesia),
+  ok.
 
 %%------------------------------------------------------------------------------
 %% @doc Lookup an {Identifier, Context} pair in the cache
 %%------------------------------------------------------------------------------
 lookup({Xi,Key} = XiKey) ->
-  case ets:lookup(?TABLE_NAME, XiKey) of
+  case mnesia:async_dirty(fun() -> mnesia:read(?TABLE_NAME, XiKey) end) of
     [] ->
       lookup({Xi,[]}, Key);
-    [{{_,_},{i,Dims,_}}] ->
+    [#?TABLE_NAME{k={_,_}, v={i,Dims,_}}] ->
       Dims;
-    [{{_,_},Value}] ->
+    [#?TABLE_NAME{k={_,_}, v=Value}] ->
       Value
   end.
 
 lookup({Xi,Key} = XiKey, K) ->
-  case ets:lookup(?TABLE_NAME, XiKey) of
+  case mnesia:async_dirty(fun() -> mnesia:read(?TABLE_NAME, XiKey) end) of
     [] ->
       [];
-    [{{_,_},{calc,W}}] ->
+    [#?TABLE_NAME{k={_,_}, v={calc,_W}=Value}] ->
       %%throw(error_calc);
-      {calc, W};
-    [{{_,Key},{i,Dims,_}}] ->
+      Value;
+    [#?TABLE_NAME{k={_,Key}, v={i,Dims,_}}] ->
       case ice_sets:restrict_domain(K, Dims) of
-	[] ->
-	  Dims;
-	K1 ->
-	  lookup({Xi,K1}, ice_sets:subtract_by_domain(K, Dims))
+        [] ->
+          Dims;
+        K1 ->
+          lookup({Xi,K1}, ice_sets:subtract_by_domain(K, Dims))
       end;
-    [{{_,_},Value}] ->
+    [#?TABLE_NAME{k={_,_}, v=Value}] ->
       Value
   end.
 
 %%------------------------------------------------------------------------------
 %% @doc Insert a value at {Xi, Key} into the cache
 %%------------------------------------------------------------------------------
-insert({Xi,Key,V}) ->
-  ets:insert(?TABLE_NAME, {{Xi,Key},V}).
+insert({_,_} = XiKey, V) ->
+  {atomic, ok} =
+    mnesia:transaction(
+      fun() ->
+          ok = mnesia:write(#?TABLE_NAME{k=XiKey, v=V})
+      end),
+  true.
 
-insert_correct_tree(Lbl) ->
-  ets:insert(Lbl, {{"A",[]}, {i,[{dim,t}],[]}}),
-  ets:insert(Lbl, {{"A",[{{dim,t},0}]}, {i,[{dim,s}],[]}}),
-  ets:insert(Lbl, {{"A",[{{dim,t},0},{{dim,s},0}]}, 1}),
-  ets:insert(Lbl, {{"A",[{{dim,t},1}]}, {i,[{dim,s}],[]}}),
-  ets:insert(Lbl, {{"A",[{{dim,t},1},{{dim,s},0}]}, 2}),
-  ets:insert(Lbl, {{"A",[{{dim,t},1},{{dim,s},1}]}, 3}),
-  ets:insert(Lbl, {{"A",[{{dim,t},2}]}, {calc,[0]}}),
-  ets:insert(Lbl, {{"B",[]}, {i,[{dim,s}],[]}}),
-  ets:insert(Lbl, {{"B",[{{dim,s},0}]}, 1}),
-  ets:insert(Lbl, {{"B",[{{dim,s},1}]}, 1}).
+%%------------------------------------------------------------------------------
+%% @doc Insert a value at {Xi, Key} into the cache unless already present
+%%------------------------------------------------------------------------------
+insert_new({_,_} = XiKey, V) ->
+  {atomic, {B, V2}} =
+    mnesia:transaction(
+      fun() ->
+          case mnesia:read(?TABLE_NAME, XiKey) of
+            [] ->
+              ok = mnesia:write(#?TABLE_NAME{k=XiKey, v=V}),
+              {true, V};
+            [#?TABLE_NAME{k=XiKey, v=V1}] ->
+              {false, V1}
+          end
+      end),
+  {B, V2}.
 
-test() ->
-  Lbl = cache_test,
-  Tab = new(),
-  insert_correct_tree(Tab),
-  [{dim,t}] = lookup({"A",[]}, Tab),
-  [{dim,s}] = lookup({"A",[{{dim,t},0}]}, Tab),
-  1 = lookup({"A",[{{dim,t},0},{{dim,s},0}]}, Tab),
-  [] = lookup({"B",[{{dim,s},2}]}, Tab),
-  [{dim,s}] = lookup({"B",[{{dim,t},0}]},Tab),
-  1 = lookup({"B",[{{dim,t},0},{{dim,s},0}]}, Tab),
-  {calc,[0]} = lookup({"A",[{{dim,t},2}]}, Tab),
-  2 = lookup({"A",[{{dim,t},1},{{dim,s},0}]}, Tab),
-  3 = lookup({"A",[{{dim,t},1},{{dim,s},1}]}, Tab).
+
+insert_correct_tree() ->
+  insert({"A",[]}, {i,[{dim,t}],[]}),
+  insert({"A",[{{dim,t},0}]}, {i,[{dim,s}],[]}),
+  insert({"A",[{{dim,t},0},{{dim,s},0}]}, 1),
+  insert({"A",[{{dim,t},1}]}, {i,[{dim,s}],[]}),
+  insert({"A",[{{dim,t},1},{{dim,s},0}]}, 2),
+  insert({"A",[{{dim,t},1},{{dim,s},1}]}, 3),
+  insert({"A",[{{dim,t},2}]}, {calc,[0]}),
+  insert({"B",[]}, {i,[{dim,s}],[]}),
+  insert({"B",[{{dim,s},0}]}, 1),
+  insert({"B",[{{dim,s},1}]}, 1).
+
+lookup_test() ->
+  ok = ?MODULE:new(),
+  insert_correct_tree(),
+  [{dim,t}] = ?MODULE:lookup({"A",[]}),
+  [{dim,s}] = ?MODULE:lookup({"A",[{{dim,t},0}]}),
+  1 = ?MODULE:lookup({"A",[{{dim,t},0},{{dim,s},0}]}),
+  [] = ?MODULE:lookup({"B",[{{dim,s},2}]}),
+  [{dim,s}] = ?MODULE:lookup({"B",[{{dim,t},0}]}),
+  1 = ?MODULE:lookup({"B",[{{dim,t},0},{{dim,s},0}]}),
+  {calc,[0]} = ?MODULE:lookup({"A",[{{dim,t},2}]}),
+  2 = ?MODULE:lookup({"A",[{{dim,t},1},{{dim,s},0}]}),
+  3 = ?MODULE:lookup({"A",[{{dim,t},1},{{dim,s},1}]}),
+  ok = ?MODULE:delete().
