ff371cafdef276c80adde21a964fd7c5b9161edf
Edward Tate <edward.tate@erlang-solutions.com>
2014-01-10 10:45:58 +0000

    Added basic Ice sequentiality tests.

---
+++ test/ice_sequence_tests.erl
@@ -0,0 +1,36 @@
+%% See the LICENSE file for licensing information
+%% -*- coding: utf-8 -*-
+-module(ice_sequence_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+ice_sequence_test_() ->
+  {foreach, fun setup/0, fun cleanup/1,
+   [
+    ?_test(sequence_parses()),
+    ?_test(sequence_evaluates())
+   ]}.
+
+%%------------------------------------------------------------------------------
+%% Test cases
+%%------------------------------------------------------------------------------
+sequence_parses() ->
+  Case1 = "1 + 2; 2 - 1",
+  ?assertMatch({ok,
+		{seq, 
+		 {primop, '+', [{int, 1}, {int, 2}]},
+		 {primop, '-', [{int, 2}, {int, 1}]}}},
+	       ice:string(Case1)).
+
+sequence_evaluates() ->
+  Case1 = "1 + 2; 2 - 1",
+  ?assertMatch({1, _}, ice:i(Case1)).
+
+%%------------------------------------------------------------------------------
+%% Internal
+%%------------------------------------------------------------------------------
+setup() -> 
+  not_implemented.
+
+cleanup(_) -> 
+  not_implemented.
