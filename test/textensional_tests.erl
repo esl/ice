20222c6e5ffd3feb261a47b99070ec895456e7fa
Pierre Fenoll <pierrefenoll@gmail.com>
2014-01-16 12:06:18 +0000

    Added a simple test but branch is too far back.

--- test/textensional_tests.erl
+++ test/textensional_tests.erl
@@ -11,7 +11,8 @@ textensional_test_() ->
   {foreach, fun setup/0, fun cleanup/1,
    [
     extensional_expr_w_missing_dims(),
-    extensional_expr_w_primops()
+    extensional_expr_w_primops(),
+    extensional_options_parser()
    ]}.
 
 
@@ -33,6 +34,34 @@ extensional_expr_w_primops() ->
         dim s <- 2
       end",
   ?_assertMatch({5,_}, eval(S)).
+
+extensional_options_parser () ->
+  S = "black_scholes_call.1.2.3.4.5
+    where
+      fun black_scholes_call.S.X.T.r.v = return where
+        var vsqrT = v * sqrt.T
+        var d1 = (log.(S/X) + (r + 0.5f0 * pow.v.2) * T) / vsqrT
+        var d2 = d1 - vsqrT
+        var return = S * cnd.d1 - X * exp.((0-r) * T) * cnd.d2
+        fun cnd.X = cnd_ret where
+          var L = abs.X
+          var K_x = 1 / (1 + 0.2316419f0 * L)
+          var K_y = pow.K_x.2
+          var K_z = pow.K_x.3
+          var K_w = pow.K_x.4
+          var dot =  0.31938153f0  * K_x
+                  -  0.356563782f0 * K_y
+                  +  1.781477937f0 * K_z
+                  -  1.821255978f0 * K_w
+          var W = (dot + 1.330274429f0 * K_w * K_x)
+                / sqrt.(2 * Pi) * exp.(-0.5f0 * pow.L.2)
+          var Pi = 3.1415926535f0
+          var cnd_ret = if X > 0
+                        then 1 - W
+                        else     W fi
+        end
+      end",
+  ?_assertMatch({42,_}, eval(S)).
 
 
 %% Internals
