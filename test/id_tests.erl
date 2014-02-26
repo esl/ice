67c5ec96ee989947012ae1200c161b0846963e28
Luca Favatella <luca.favatella@erlang-solutions.com>
2014-02-26 08:30:43 +0000

    Move not strictly needed tests in extra_tests test suite...
    ... in order to start easing the burden of maintaining the unit tests.

--- test/id_tests.erl
+++ test/id_tests.erl
@@ -166,55 +166,6 @@ complex_ids_test_() ->
   {setup, fun setup/0, fun cleanup/1,
    ?_assertMatch({31,_}, eval(S))}. %% Upstream TL returns 31
 
-transform1_rules_test_() ->
-  Consts = [{int, 46},
-            {bool, false},
-            {string,"ciao"}],
-  ConstTests = lists:zip(Consts, Consts),
-  WheredimTest = {WheredimTree, WheredimExpected} =
-    { {where,    {'#',            {id,"t"}}, [{ dim,       {id,"t"} ,{int,46}}]},
-      {wheredim, {'#',{dim,{[],1},    "t"}}, [{{dim,{[],1},    "t" },{int,46}}]} },
-  WheredimTreeF =
-    fun(DimName) when is_list(DimName) ->
-        {where,    {'#',             {id,DimName}}, [ {dim,        {id,DimName} ,{int,46}}]}
-    end,
-  WheredimExpectedF =
-    fun(DimName, Pos) when is_list(DimName), is_list(Pos) ->
-        {wheredim, {'#',{dim,{Pos,1},    DimName}}, [{{dim,{Pos,1},    DimName },{int,46}}]}
-    end,
-  WheredimTree     = WheredimTreeF(    "t"),
-  WheredimExpected = WheredimExpectedF("t", []),
-  WherevarTest =
-    { {where,    WheredimTree,
-       [{var,{id,"X"},WheredimTree              },
-        {var,{id,"Y"},WheredimTree              }]},
-      {wherevar, WheredimExpectedF("t",[0]),
-       [{    {id,"X"},WheredimExpectedF("t",[1])},
-        {    {id,"Y"},WheredimExpectedF("t",[2])}]} },
-  %% Testing expression in dimensional query, even if it does not make
-  %% sense as dims are not ground values atm
-  DimQueryTest =
-    { {'#', WheredimTree    },
-      {'#', WheredimExpected} },
-  TupleTests =
-    [{ {t, [{{id,"lhs"},{int,46}}]},
-       {t, [{{id,"lhs"},{int,46}}]} },
-     { {t, [{{id,"lhs1"},WheredimTree              },
-            {{id,"lhs2"},WheredimTree              }]},
-       {t, [{{id,"lhs1"},WheredimExpectedF("t",[3])},
-            {{id,"lhs2"},WheredimExpectedF("t",[5])}]} }
-    ],
-  %% TODO (not important) test perturbation, primop, if-then-else
-  TreeExpectedTuples =
-    ConstTests ++ [WheredimTest, WherevarTest, DimQueryTest] ++ TupleTests,
-  [?_test(begin
-            AstAfterT0 = t0(Tree),
-            Actual = t1(AstAfterT0),
-            %io:format(user, "AstAfterT0: ~1000p~nActual: ~1000p~n", [AstAfterT0, Actual]),
-            ?assertEqual(Expected, Actual)
-          end)
-   || {Tree, Expected} <- TreeExpectedTuples].
-
 
 dim_id_can_be_assigned_to_var() ->
   ?assertMatch({46,_}, eval("#.A where var A = d;; dim d <- 46 end")).
