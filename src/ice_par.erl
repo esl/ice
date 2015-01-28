95a7ce69b91a25bb4820413fae5e96c5f76bb2cc
Edward Tate <edward.tate@erlang--solutions.com>
2015-01-28 16:04:12 +0000

    Removed T from ice_par.

--- src/ice_par.erl
+++ src/ice_par.erl
@@ -3,7 +3,7 @@
 %%-------------------------------------------------------------------------------------
 -module(ice_par).
 
--export([eval/7, eval_seq/7]).
+-export([eval/6, eval_seq/6]).
 
 %%------------------------------------------------------------------------------
 %% @doc Evaluate expressions in parallel
@@ -11,31 +11,26 @@
 %% The order of the evaluated results is guaranteed to be the same as
 %% the order of the expressions specified.
 %% ------------------------------------------------------------------------------
-eval(Xs, _I, _E, _K, _D, _W, _T) when length(Xs) == 0 ->
-  {Xs, 0};
-eval(Xs, I, E, K, D, W, T) ->
+eval(Xs, _I, _E, _K, _D, _W) when length(Xs) == 0 ->
+  Xs;
+eval(Xs, I, E, K, D, W) ->
   %%------------------------------------------------------------------------------
   %% Note: Passing self() here means W is self()
   %%------------------------------------------------------------------------------
   Pids = ice_thread:spawn_n(W, length(Xs)),
-  ice_thread:join(Pids, Xs, I, E, K, D, W, T).
+  ice_thread:join(Pids, Xs, I, E, K, D, W).
 
 %%-------------------------------------------------------------------------------------
 %% @doc Evaluate a sequence expressions 
 %%-------------------------------------------------------------------------------------
-eval_seq(Xs, I, E, K, D, W, T) ->
-  eval_seq(Xs, I, E, K, D, W, T, []).
+eval_seq(Xs, I, E, K, D, W) ->
+  eval_seq(Xs, I, E, K, D, W, []).
 
-eval_seq([], I, E, K, D, W, T, Acc) ->
-  {lists:reverse(Acc), T};
-eval_seq([X|Xs], I, E, K, D, W, T, Acc) ->
-  {D0, T1} = ice_core:eval(X, I, E, K, D, W, T),
-  case T1 > T of
-    true ->
-      eval_seq(Xs, I, E, K, D, W, T1, [D0|Acc]);
-    false ->
-      eval_seq(Xs, I, E, K, D, W, T, [D0|Acc])
-  end.
+eval_seq([], _I, _E, _K, _D, _W, Acc) ->
+  lists:reverse(Acc);
+eval_seq([X|Xs], I, E, K, D, W, Acc) ->
+  D0 = ice_core:eval(X, I, E, K, D, W),
+  eval_seq(Xs, I, E, K, D, W, [D0|Acc]).
 
 
 
